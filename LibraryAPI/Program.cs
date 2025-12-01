67b4d7f2263c248bde75028aabd624b34be9932b
Dejmenek <dejmen14567@gmail.com>
2025-12-01 16:39:50 +0100

    feat: add book management endpoints and OpenAPI support
    This commit introduces several using directives for necessary namespaces, including validation support. It adds OpenAPI configuration for better API documentation. A new route group for books is created, organizing endpoints for retrieving, creating, updating, and deleting books. Each HTTP method is implemented with appropriate validation and response handling, enhancing the application's functionality and maintainability.

--- LibraryAPI/Program.cs
+++ LibraryAPI/Program.cs
@@ -1,5 +1,10 @@
 using LibraryAPI.Data;
+using LibraryAPI.Helpers;
+using LibraryAPI.Models;
+using LibraryAPI.Requests;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
@@ -11,6 +16,64 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+var books = app.MapGroup("/books");
+
+books.MapGet("/", async (ApplicationDbContext context) =>
+{
+    var books = await context.Books.Include(b => b.Author).AsNoTracking().ToListAsync();
+    return books.ToBooksWithAuthorsResponse();
+});
+
+books.MapPost("/{id}", async (int id, [FromBody] BookRequest request, ApplicationDbContext context) =>
+{
+    var validationResults = new List<ValidationResult>();
+
+    var validationContext = new ValidationContext(request);
+
+    if (!Validator.TryValidateObject(request, validationContext, validationResults, true))
+    {
+        var errors = validationResults.ToDictionary(
+                    v => v.MemberNames.FirstOrDefault() ?? "Error",
+                    v => new string[] { v.ErrorMessage! });
+
+        return Results.BadRequest(new { Message = "Validation failed", Errors = errors });
+    }
+
+    var book = request.ToBook();
+
+    context.Books.Add(book);
+    await context.SaveChangesAsync();
+
+    return Results.Created($"/books/{book.Id}", book);
+});
+
+books.MapPut("/{id}", async (int id, [FromBody] BookRequest request, ApplicationDbContext context) =>
+{
+    var book = await context.Books.FindAsync(id);
+
+    if (book is null) return Results.NotFound();
+
+    book.Year = request.Year;
+    book.Title = request.Title;
+    book.AuthorId = request.AuthorId;
+
+    await context.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+books.MapDelete("/{id}", async (int id, ApplicationDbContext context) =>
+{
+    if (await context.Books.FindAsync(id) is Book book)
+    {
+        context.Books.Remove(book);
+        await context.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
